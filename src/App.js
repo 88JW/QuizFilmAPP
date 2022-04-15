d1a5668bf5b000432e5e25d529b8f0b88a1533ba
WJ <w.jaskula8@gmail.com>
2022-04-15 21:10:31 +0200

    master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,123 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { QuizData } from "./QuizData";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  state = {
+    userAnswer: null,
+    currentIndex: 0,
+    option: [],
+    quizEnd: false,
+    score: 0,
+    disable: true,
+  };
+
+  loadQuiz = () => {
+    const { currentIndex } = this.state;
+    this.setState(() => {
+      return {
+        question: QuizData[currentIndex].question,
+        option: QuizData[currentIndex].options,
+        answer: QuizData[currentIndex].answer,
+      };
+    });
+  };
+
+  nextQuestionHadler = () => {
+    const { userAnswer, answer, score } = this.state;
+
+    if (userAnswer === answer) {
+      this.setState({
+        score: score + 1,
+      });
+    }
+
+    this.setState({
+      currentIndex: this.state.currentIndex + 1,
+      userAnswer: null,
+    });
+  };
+
+  componentDidMount() {
+    this.loadQuiz();
+  }
+
+  chackAnswer = (answer) => {
+    this.setState({
+      userAnswer: answer,
+      disable: false,
+    });
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const { currentIndex } = this.state;
+    if (this.state.currentIndex != prevState.currentIndex) {
+      this.setState(() => {
+        return {
+          question: QuizData[currentIndex].question,
+          option: QuizData[currentIndex].options,
+          answer: QuizData[currentIndex].answer,
+        };
+      });
+    }
+  }
+
+  finishHandler = () => {
+    if (this.state.currentIndex === QuizData.length - 1) {
+      this.setState({
+        quizEnd: true,
+      });
+    }
+  };
+
+  render() {
+    const { question, option, currentIndex, userAnswer, quizEnd, score } =
+      this.state;
+
+    if (quizEnd) {
+      return (
+        <div>
+          <h1>Koniec gry twój wynik to: {this.state.score} </h1>
+
+          <p>
+            {this.state.score >= 3
+              ? "Możesz zalogować kesza"
+              : "nie dostaniesz hasła"}
+          </p>
+        </div>
+      );
+    }
+
+    return (
+      <div>
+        <h2>{question}</h2>
+        <span>{`Pytanie ${currentIndex + 1} z ${QuizData.length} `}</span>
+        {option.map((option) => (
+          <p
+            key={option.id}
+            className={`options ${userAnswer === option ? "selected" : null}`}
+            onClick={() => this.chackAnswer(option)}
+          >
+            {" "}
+            {option}{" "}
+          </p>
+        ))}
+
+        {currentIndex < QuizData.length - 1 && (
+          <button
+            disabled={this.state.disable}
+            onClick={this.nextQuestionHadler}
+          >
+            Następne pytanie
+          </button>
+        )}
+        {currentIndex === QuizData.length - 1 && (
+          <button disabled={this.state.disabled} onClick={this.finishHandler}>
+            Zaznacz i prześlij wynik
+          </button>
+        )}
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/QuizData.js
@@ -0,0 +1,67 @@
+import one from "./img/2_forrest_gump.PNG";
+import two from "./img/3_darjeeling_limited.PNG";
+import tree from "./img/4_fight_club.PNG";
+import four from "./img/5_life_of_brian.PNG";
+import five from "./img/6_monty_python_and_the_holy_grail.PNG";
+import six from "./img/7_dr_strangelove.PNG";
+import seven from "./img/8_batman.PNG";
+import eight from "./img/9_rocky_horror_picture_show.PNG";
+import nine from "./img/10_pulp_fiction.PNG";
+
+export const QuizData = [
+  {
+    id: 0,
+    question: <img src={one} />,
+    options: [`FOREST GUMP`, `SENS ŻYCIA`, `SIEDMIU SAMURAJÓW`, `ŁOWCA JELENI`],
+    answer: `Meaning of life`,
+  },
+  {
+    id: 1,
+    question: <img src={two}></img>,
+    options: [`Forest Gump`, `X1`, `X2`, `X3`],
+    answer: `Forest Gump`,
+  },
+  {
+    id: 2,
+    question: <img src={tree}></img>,
+    options: [`Darjeeling Limited`, `X1`, `X2`, `X3`],
+    answer: `Darjeeling Limited`,
+  },
+
+  {
+    id: 3,
+    question: <img src={four}></img>,
+    options: [`Fight Club`, `X1`, `X2`, `X3`],
+    answer: `Fight Club`,
+  },
+  {
+    id: 4,
+    question: <img src={five}></img>,
+    options: [`Rimadi`, `Damascus`, `Owerri`, `Tel Aviv`],
+    answer: `Damascus`,
+  },
+  {
+    id: 5,
+    question: <img src={six}></img>,
+    options: [`Rimadi`, `Damascus`, `Owerri`, `Tel Aviv`],
+    answer: `Damascus`,
+  },
+  {
+    id: 6,
+    question: <img src={seven}></img>,
+    options: [`Rimadi`, `Damascus`, `Owerri`, `Tel Aviv`],
+    answer: `Damascus`,
+  },
+  {
+    id: 7,
+    question: <img src={eight}></img>,
+    options: [`Rimadi`, `Damascus`, `Owerri`, `Tel Aviv`],
+    answer: `Damascus`,
+  },
+  {
+    id: 7,
+    question: <img src={nine}></img>,
+    options: [`Rimadi`, `Damascus`, `Owerri`, `Tel Aviv`],
+    answer: `Damascus`,
+  },
+];
